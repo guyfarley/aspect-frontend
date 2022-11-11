93e6dcd5da883598da353eda17f418791db3c872
Guy Farley <guy.e.farley@gmail.com>
2022-11-11 14:47:07 -0800

    Hero image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
     <div className="relative h-screen lg:h-[140vh]">
       <Head>
