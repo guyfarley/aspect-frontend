74ba77f89bd92347c2040d03ef694e6dc7833232
Guy Farley <guy.e.farley@gmail.com>
2022-11-14 14:48:28 -0800

    Basic styling of home page

---
+++ components/Menu.tsx
@@ -0,0 +1,15 @@
+function Menu() {
+  return (
+    // <div className="mt-[16vh] px-4 py-4">
+    <div className="flex justify-center">
+      <ul className="mt-[16vh] md:flex flex-row justify-around">
+        <li className="menuLink">Find an Install</li>
+        <li className="menuLink">Create an Install</li>
+        <li className="menuLink">Campaign Report</li>
+      </ul>
+
+    </div>
+  )
+}
+
+export default Menu;
