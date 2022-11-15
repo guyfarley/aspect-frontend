892c68533cb38eed3b42b0ff561ad9acafb5d3d1
Guy Farley <guy.e.farley@gmail.com>
2022-11-14 20:05:11 -0800

    Button styling

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,12 +1,10 @@
 function Menu() {
   return (
-    // <div className="mt-[16vh] px-4 py-4">
-    <div className="flex justify-center">
-      <ul className="mt-[16vh] md:flex flex-row justify-around">
-        <li className="menuLink">Find an Install</li>
-        <li className="menuLink">Create an Install</li>
-        <li className="menuLink">Campaign Report</li>
-      </ul>
+    <div className="flex flex-col md:flex-row justify-center mt-[16vh]">
+      <button className="menuButton">Log In</button>
+      <button className="menuButton">Find an Install</button>
+      <button className="menuButton">Create an Install</button>
+      <button className="menuButton">Campaign Report</button>
 
     </div>
   )
