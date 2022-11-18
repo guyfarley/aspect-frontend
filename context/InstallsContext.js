8ac4bbcb51010d3a7ec4b2ff9d9a36bb0c92ef08
Guy Farley <guy.e.farley@gmail.com>
2022-11-18 13:57:19 -0800

    More context

--- context/InstallsContext.js
+++ context/InstallsContext.js
@@ -5,12 +5,33 @@ const InstallsContext = createContext();
 const InstallsProvider = ({ children }) => {
 
   const [installs, setInstalls] = useState([]);
+  // const [formData, setFormData] = useState({
+  //   id: install.id,
+  //   pm: install.pm,
+  //   storeNum: install.storeNum,
+  //   location: install.location,
+  //   campaign: install.campaign,
+  //   vendorName: install.vendorName,
+  //   vendorPhone: install.vendorPhone,
+  //   installDate: install.installDate,
+  //   installTime: install.installTime,
+  //   installer: install.installer,
+  //   installerPhone: install.installerPhone,
+  //   installerNotes: install.installerNotes,
+  //   complete: install.complete,
+  //   completionPics: install.completionPics,
+  //   revisitNeeded: install.revisitNeeded,
+  //   revisitDate: install.revisitDate,
+  //   pmNotes: install.pmNotes,
+  // });
+  // const [formData, setFormData] = useState({});
+
 
   return (
     <InstallsContext.Provider
       value={{
         installs,
-        setInstalls
+        setInstalls,
       }
       }
     >
