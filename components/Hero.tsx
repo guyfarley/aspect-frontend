7a5355bfe380c27141ccee6009af6ac28869394e
Guy Farley <guy.e.farley@gmail.com>
2022-12-08 11:51:38 -0800

    Index font styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 function Hero() {
   return (
     <div className="relative h-[45vh] w-[100vw] px-4 py-4 mt-20 md:h-[30vh] md:w-[80vw]">
+      <h1 className="font-ptserif text-gray-700 text-4xl">See your marketing,</h1>
+      <h1 className="font-ptserif text-gray-700 text-4xl mb-4">delivered.</h1>
       <Image
         src="/deschutes_resized.JPG"
         alt="Deschutes River"
