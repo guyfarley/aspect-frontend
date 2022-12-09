f613582c2fb328321227cf03bc57dd8a97b3662d
Guy Farley <guy.e.farley@gmail.com>
2022-12-08 16:15:05 -0800

    Prisma type solve

--- db.ts
+++ db.ts
@@ -1,13 +1,31 @@
 import { PrismaClient } from '@prisma/client';
 
-// instantiates one instance of prisma to be used across entire app
+let prisma: PrismaClient;
 
-declare global {
-  const prisma: PrismaClient | undefined
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  let globalWithPrisma = global as typeof globalThis & {
+    prisma: PrismaClient;
+  };
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient();
+  }
+  prisma = globalWithPrisma.prisma;
 }
 
-export const prisma =
-  global.prisma ||
-  new PrismaClient();
+export default prisma;
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+// import { PrismaClient } from '@prisma/client';
+
+// // instantiates one instance of prisma to be used across entire app
+
+// declare global {
+//   const prisma: PrismaClient | undefined
+// }
+
+// export const prisma =
+//   global.prisma ||
+//   new PrismaClient();
+
+// if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
