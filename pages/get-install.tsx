40eeaa300944de1e5cbe56e5e4968e8718e47d71
Guy Farley <guy.e.farley@gmail.com>
2022-12-10 17:36:27 -0800

    Styling

--- pages/get-install.tsx
+++ pages/get-install.tsx
@@ -25,20 +25,18 @@ export default withPageAuthRequired(function GetInstallForm() {
   // validate store number exists here?
 
   return (
-    <>
+    <div className="relative flex justify-center h-screen px-4 py-4 lg:h-[140vh]  bg-slate-200">
       <Header />
-      <div className="h-full w-full bg-slate-200">
-        <div className="flex flex-col h-full w-full bg-slate-200 p-8 md:max-w-lg md:mx-auto md:items-start">
+      <div className="flex flex-col h-full w-full mt-[100px] md:max-w-lg md:mx-auto md:items-start">
 
-          <h1 className="font-ptserif text-gray-700 text-4xl mt-20">Looking for a specific install?</h1>
-          <h1 className="font-roboto text-gray-700 text-base mt-[20px]">Just enter your store number below and submit!</h1>
-          <form className="flex flex-col items-center mt-[15vh] mb-[70vh] md:items-start" onSubmit={handleSubmit}>
-            <input className="w-[130px] border rounded pl-[6px] py-[3px]" type="text" id="storeNumber" name="storeNum" onChange={(e) => setRoute(e.target.value)} placeholder="Store #" required />
-            <button className="submitButton" type="submit">Submit</button>
-          </form>
-        </div>
+        <h1 className="font-ptserif text-gray-700 text-4xl">Looking for a specific install?</h1>
+        <h1 className="font-roboto text-gray-700 text-base mt-[16px]">Just enter your store number below and submit!</h1>
+        <form className="flex flex-col items-center mt-[15vh] mb-[70vh] md:items-start" onSubmit={handleSubmit}>
+          <input className="w-[130px] border rounded pl-[6px] py-[3px]" type="text" id="storeNumber" name="storeNum" onChange={(e) => setRoute(e.target.value)} placeholder="Store #" required />
+          <button className="submitButton" type="submit">Submit</button>
+        </form>
       </div>
       {/* footer */}
-    </>
+    </div>
   )
 })
