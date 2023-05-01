e7f65408988215d5d2b55d09dd16c42d647f82da
Guy Farley <guy.e.farley@gmail.com>
2023-05-01 11:40:34 -0700

    componentized home page

---
+++ components/AccessInstalls.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import Footer from './Footer';
+import Link from 'next/link';
+
+function AccessInstalls() {
+  return (
+    <div className="flex flex-col justify-center w-[100vw] px-6 py-4 md:w-[60vw]">
+      <h1 className="font-ptserif text-gray-700 text-4xl mt-[40px]">Access your installs.</h1>
+      <h3 className="font-roboto text-gray-600 text-base mt-[30px] mb-[30px]">To access an installation, visit the <Link href="/get-install">FIND INSTALL</Link> page and choose from the available marketing campaigns. The CHOOSE INSTALL dropdown menu will auto-populate with all installations related to that campaign. Choose the correct install location and submit, and you'll be taken to a new page showing all current data for that installation! From there, you may MODIFY or DELETE the install as needed. It's that easy!</h3>
+      <div className="shadow-lg">
+        <Image
+          src="/laptop_1.jpg"
+          alt="laptop on desk"
+          width="1280"
+          height="853"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default AccessInstalls;
---
+++ components/GetReport.tsx
@@ -0,0 +1,54 @@
+import Image from 'next/image'
+import Footer from './Footer';
+import Link from 'next/link';
+import React, { useState, useEffect, useContext } from 'react';
+import { useRouter } from 'next/router';
+
+interface Campaigns {
+  campaigns: string[]
+}
+
+const GetReport = ({ campaigns }: Campaigns): JSX.Element => {
+
+  const router = useRouter();
+  const [route, setRoute] = useState("");
+
+  // calls getInstalls function to get filtered-down installs, sets those to filteredInstalls
+  const handleCampaignSelection = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    event.preventDefault();
+    const target = event.target as HTMLSelectElement;
+    setRoute(target.value);
+  }
+
+  // when Submit button clicked, user routed to dynamic route for the chosen install
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    router.push(`/reports/${route}`);
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center mt-[20px] w-[100vw] px-6 py-4 md:w-[60vw] md:items-start">
+      <h1 className="font-ptserif text-gray-700 text-4xl">Get full campaign report.</h1>
+      <form className="flex flex-col items-center mt-[50px] md:items-start" onSubmit={handleSubmit}>
+        <select
+          className="w-[180px] border rounded pl-[6px] py-[3px] mb-2 md:ml-2"
+          name="campaigns"
+          id="campaigns"
+          onChange={handleCampaignSelection}
+        >
+          <option className="font-roboto text-gray-400">Select Campaign</option>
+          {campaigns.map((campaign) => (
+            <option className="font-roboto text-gray-600" key={campaign}>
+              {campaign}
+            </option>
+          ))}
+        </select>
+        <div className="md:ml-2">
+          <button className="submitButton" type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default GetReport;
